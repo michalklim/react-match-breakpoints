f6bb1b6e27500551f9f5aae46294f7ae1c995fdb
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2018-09-05 22:37:25 +0200

    chore: move jest configuration to package.json

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,7 +5,3 @@ require('matchmedia-polyfill')
 require('matchmedia-polyfill/matchMedia.addListener')
 
 configure({ adapter: new Adapter() })
-
-module.exports = {
-  testMatch: ['<rootDir>/src/**/__tests__/**/*.{js,jsx,mjs}', '<rootDir>/src/**/?(*.)(spec|test).{js,jsx,mjs}'],
-}
