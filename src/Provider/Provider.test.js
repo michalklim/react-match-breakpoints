9ca7b5d2814aa11235abac8382b31f1395049cb9
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2018-09-11 20:58:58 +0200

    test(provider): update tests

--- src/Provider/Provider.test.js
+++ src/Provider/Provider.test.js
@@ -22,15 +22,11 @@ const stateMediaBreakpoints = {
   isDesktop: false,
 }
 
-beforeEach(() => {
-  jest.resetModules()
-})
-
 describe('<Provider />', () => {
-  it('should render Provider', () => {
+  it('renders without crashing', () => {
     shallow(
       <Provider breakpoints={breakpoints}>
-        <div>test</div>
+        <div />
       </Provider>
     )
   })
@@ -38,21 +34,92 @@ describe('<Provider />', () => {
   it('matches the snapshot', () => {
     const wrapper = shallow(
       <Provider breakpoints={breakpoints}>
-        <div>test</div>
+        <div />
       </Provider>
     )
     expect(toJson(wrapper)).toMatchSnapshot()
   })
+
+  it('it passes breakpoints state to context provider', () => {
+    const wrapper = shallow(
+      <Provider breakpoints={breakpoints}>
+        <div />
+      </Provider>
+    )
+
+    expect(wrapper.instance().state).toEqual({ ...stateMediaBreakpoints })
+    expect(wrapper.find('ContextProvider').prop('value')).toEqual({ ...stateMediaBreakpoints })
+  })
+})
+
+describe('<Provider /> buildMatchMediaBreakpoints method', () => {
+  it('returns object with matchMedia values for breakpoints when provided user breakpoints', () => {
+    const wrapper = shallow(
+      <Provider breakpoints={breakpoints}>
+        <div />
+      </Provider>
+    )
+    const returnObject = wrapper.instance().buildMatchMediaBreakpoints(breakpoints)
+    const getMatchMediaObject = breakpointMedia => ({
+      addListener: expect.any(Function),
+      removeListener: expect.any(Function),
+      media: breakpointMedia,
+      matches: expect.any(Boolean),
+    })
+
+    const breakpointsKeys = Object.keys(breakpoints)
+
+    expect(returnObject).toEqual({
+      [breakpointsKeys[0]]: getMatchMediaObject(breakpoints[breakpointsKeys[0]]),
+      [breakpointsKeys[1]]: getMatchMediaObject(breakpoints[breakpointsKeys[1]]),
+      [breakpointsKeys[2]]: getMatchMediaObject(breakpoints[breakpointsKeys[2]]),
+    })
+  })
 })
 
 describe('<Provider /> buildBooleanBreakpoints method', () => {
-  it('should return object with boolean values for breakpoints when provided mq values', () => {
+  it('returns object with boolean values for breakpoints when provided matchMedia values', () => {
     const wrapper = shallow(
       <Provider breakpoints={breakpoints}>
-        <div>test</div>
+        <div />
       </Provider>
     )
     const returnObject = wrapper.instance().buildBooleanBreakpoints(matchMediaBreakpoints)
     expect(returnObject).toEqual(stateMediaBreakpoints)
+  })
+})
+
+describe('<Provider /> setActiveBreakpoint method', () => {
+  it('set given breakpoint in state to true', () => {
+    const wrapper = shallow(
+      <Provider breakpoints={breakpoints}>
+        <div />
+      </Provider>
+    )
+
+    expect(wrapper.instance().state).toEqual({ ...stateMediaBreakpoints })
+
+    wrapper.instance().setActiveBreakpoint('isMobile')
+
+    expect(wrapper.instance().state).toEqual({ ...stateMediaBreakpoints, isMobile: true })
+  })
+})
+
+describe('<Provider /> unsetActiveBreakpoint method', () => {
+  it('set given breakpoint in state to false', () => {
+    const wrapper = shallow(
+      <Provider breakpoints={breakpoints}>
+        <div />
+      </Provider>
+    )
+
+    wrapper.setState({
+      ...stateMediaBreakpoints,
+      isMobile: true,
+    })
+
+    wrapper.instance().unsetActiveBreakpoint('isMobile')
+
+    expect(wrapper.instance().state).toEqual({ ...stateMediaBreakpoints, isMobile: false })
   })
 })
