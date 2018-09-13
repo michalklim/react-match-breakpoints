96cd73c913e5c3c86be10460905cf32b7bd39840
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2018-09-13 23:20:24 +0200

    feat: export proxied breakpoints only if Proxy is supported

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Provider from './Provider'
 import withBreakpoints from './withBreakpoints'
-import { proxiedBreakpointsStoreInstance } from './Breakpoints'
+import { proxiedBreakpointsStoreInstance, breakpointsStoreInstance } from './Breakpoints'
 import createBreakpoints from './createBreakpoints'
 import Context from './Context'
 
-export { Provider, withBreakpoints, proxiedBreakpointsStoreInstance as Breakpoints, createBreakpoints, Context }
+const Breakpoints = (window.Proxy && proxiedBreakpointsStoreInstance) || breakpointsStoreInstance
+
+export { Provider, withBreakpoints, Breakpoints, createBreakpoints, Context }
