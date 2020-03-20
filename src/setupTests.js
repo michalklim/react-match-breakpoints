76f993ddb6d990c5724081d2f6af4bc6cfcbe104
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-03-20 22:17:50 +0100

    chore: setup jest tests

--- src/setupTests.js
+++
@@ -1,7 +0,0 @@
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
-
-require('matchmedia-polyfill')
-require('matchmedia-polyfill/matchMedia.addListener')
-
-configure({ adapter: new Adapter() })
