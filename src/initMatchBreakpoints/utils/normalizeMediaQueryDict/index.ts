45db57f4179d2d747d89d13829e7cdf6a1cebc61
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-03-21 22:24:58 +0100

    refactor: remove unnecessary global types export

--- src/initMatchBreakpoints/utils/normalizeMediaQueryDict/index.ts
+++ src/initMatchBreakpoints/utils/normalizeMediaQueryDict/index.ts
@@ -1,4 +1,1 @@
-import { NormalizedMediaQueryDict as ImportedNormalizedMediaQueryDict } from './types'
-
 export { normalizeMediaQueryDict } from './normalizeMediaQueryDict.util'
-export type NormalizedMediaQueryDict = ImportedNormalizedMediaQueryDict
