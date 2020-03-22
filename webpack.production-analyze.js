37a9622d2c52c76621d96d2e4f9f3f5e278bea22
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-03-22 21:16:41 +0100

    chore: setup build and dev scripts

---
+++ webpack.production-analyze.js
@@ -0,0 +1,8 @@
+const webpackMerge = require('webpack-merge')
+const productionConfig = require('./webpack.production')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
+
+module.exports = () =>
+  webpackMerge(productionConfig, {
+    plugins: [new BundleAnalyzerPlugin()],
+  })
--- webpack.production.js
+++ webpack.production.js
@@ -1,14 +1,35 @@
+const webpack = require('webpack')
 const pkg = require('./package.json')
+const webpackMerge = require('webpack-merge')
+const typescriptConfig = require('./webpack.typescript-base')
 
-module.exports = () => ({
-  mode: 'production',
-  devtool: 'source-maps',
-  output: {
-    filename: 'index.js',
-    libraryTarget: 'umd',
-    library: pkg.name,
-  },
-  externals: {
-    react: 'react',
-  },
-})
+module.exports = () =>
+  webpackMerge(
+    {
+      plugins: [new webpack.ProgressPlugin()],
+      mode: 'production',
+      devtool: 'source-maps',
+      output: {
+        filename: 'index.js',
+        libraryTarget: 'umd',
+        library: pkg.name,
+      },
+      module: {
+        rules: [
+          {
+            enforce: 'pre',
+            test: /\.(ts|tsx)$/,
+            exclude: /node_modules/,
+            loader: 'eslint-loader',
+            options: {
+              emitError: true,
+            },
+          },
+        ],
+      },
+      externals: {
+        react: 'react',
+      },
+    },
+    typescriptConfig,
+  )
