2a27a6fc8bc6e2ea2ee8609e5a5adcee700acdf9
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-03-24 00:26:51 +0100

    chore(eslint): turn of @typescript-eslint/explicit-function-return-type

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   rules: {
     'react/prop-types': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
     // e.g. "@typescript-eslint/explicit-function-return-type": "off",
   },
