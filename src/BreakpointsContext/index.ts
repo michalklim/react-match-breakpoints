3ef07f36dafd093cae8ae2ca720a8d514000940a
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2020-03-24 00:04:21 +0100

    refactor: return denormalized state from use and withBreakpoints

--- src/BreakpointsContext/index.ts
+++ src/BreakpointsContext/index.ts
@@ -1,3 +1,5 @@
 import { createContext } from 'react'
 
 export const BreakpointsContext = createContext<BreakpointsState>({})
+export { generateInitialState } from './utils/generateInitialState'
+export { denormalizeBreakpointsState } from './utils/denormalizeBreakpointsState.util.ts'
