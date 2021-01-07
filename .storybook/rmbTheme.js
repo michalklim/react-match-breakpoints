538a2bd875187c70a5960ebabf5345553ea9ab10
Michał Wołczecki-Klim <michal.wklim@gmail.com>
2021-01-07 23:05:56 +0100

    docs: update initial docs

--- .storybook/rmbTheme.js
+++ .storybook/rmbTheme.js
@@ -3,6 +3,6 @@ import { create } from '@storybook/theming/create'
 export default create({
   base: 'light',
   brandTitle: 'React Match Breakpoints',
-  brandUrl: 'https://example.com',
-  brandImage: 'https://placehold.it/350x150',
+  brandUrl: 'https://michalklim.github.io/react-match-breakpoints/',
+  brandImage: 'https://raw.githubusercontent.com/michalklim/react-match-breakpoints/master/rmb-simple-logo.png',
 })
